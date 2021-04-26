dbc0cc7104a7154d006d8697b1a90e86a4cb946f
JohnDamiano <82375854+JohnDamiano@users.noreply.github.com>
2021-04-26 10:34:17 +0800

    Add files via upload

---
+++ TeamTaskManagementSystem.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class TeamTaskManagementSystem
+{
+    static public void Main ()
+    {
+     Console.WriteLine("Team Task Manager");
+     string Member1name = "John A. nhel Damiano";
+string Project = "Team Task Management System";
+     string Assignment = "Debugger";
+string password = "Damianojohn";
+string possition = "Team leader";
+    
+    Console.WriteLine("Enter Your Position");
+    String Possition = Console.ReadLine();
+    
+    }
+
+}        
